c1cf81e1dbe99fb4d22ea1ce869509657fdbdb28
Alessandro <aleminipublic@gmail.com>
2021-12-02 21:13:53 +0100

    Added some comments and fixed some errors

--- minhash.hpp
+++ minhash.hpp
@@ -1,84 +1,88 @@
 #include <bits/stdc++.h>
 
-const int K=322; //number of functions required by a good estimator (epsilon=0.8, delta=1e-9)
-const int MAXU=1e6; //max possible number in the universe considered
-const int M=1e9; //we need M>MAXU
+const int K=70; //Number of functions required by a good estimator (epsilon=0.3, delta=1e-6)
+const int MAXU=1e6; //Max possible number in the universe considered
+const int M=1e9+7; //We need M>MAXU and M must be prime to guarantee collision-free propriety to hash functions
 
-//This class stores k hash functions and generates sketches for sets
-class Sketch{
+//This class stores k hash functions for sketches
+class Hash{
 private:
-
   int a[K];
   int b[K];
 
 public:
-  Sketch(){
+  Hash(){
     std::random_device rd_a;
     std::mt19937 gen_a(rd_a());
-    std::uniform_int_distribution<int> distr_a(1,M-1);
+    std::uniform_int_distribution<int> distr_a(1,M-1); //coefficients must be uniformly distributed
 
     std::random_device rd_b;
     std::mt19937 gen_b(rd_b());
-    std::uniform_int_distribution<int> distr_b(0,M-1);
-    for(int i=0;i<K;i++)a[i]=distr_a(gen_a); //Generation of a coefficients for hash functions
-    for(int i=0;i<K;i++)b[i]=distr_b(gen_b); //Generation of b coefficients for hash functions
+    std::uniform_int_distribution<int> distr_b(0,M-1); //coefficients must be uniformly distributed
+
+    for(int i=0;i<K;i++)a[i]=distr_a(gen_a); //Generate a_i coefficients for hash functions
+    for(int i=0;i<K;i++)b[i]=distr_b(gen_b); //Generate b_i coefficients for hash functions
   }
   int hash(int i, int x){  //hash functions are simply h_i(x)=a_i*x+b_i mod M
-    return ((long long)x*a[i]+b[i])%M;
+    return ((long long)(x+1)*a[i]+b[i])%M; //x+1 allows to have an hash function with domain [1,MAXU]
   }
 };
 
-//Data Structure that store sketch for min hash and the set itself
+//Data Structure that store sketch for min hash (it can also contain the set itself)
 //elements are integers for simplicity
 class Set{
 
 private:
   int sz;
-  int sketch[K];
-  Sketch *sk;
+  int sketch[K]; //It's safer keep sketch private to avoid user changes
+  Hash *h;
 
 public:
-  Set(Sketch *skk, std::set<int> a){
-    sk=skk;  //save Sketch ptr for further operations
+  Set(Hash *h, std::set<int> a){
+    for(int x:a)assert(x>=0&&x<MAXU);
+    this->h=h;  //save Hash ptr for further operations
     sz=a.size();
     for(int i=0;i<K;i++){  //Calculate MinHash for every of the k hash functions
       int min_h=M-1;
-      for(int x:a)min_h=std::min(min_h, (*sk).hash(i,x));
+      for(int x:a)min_h=std::min(min_h, (*h).hash(i,x));
+
       sketch[i]=min_h;
     }
   }
-  Set(Sketch *skk){
-    sk=skk;  //save Sketch ptr for further insertions
+  Set(Hash *h){  //creates an empty set
+    this->h=h;  //save Hash ptr for further operations
     sz=0;
     for(int i=0;i<K;i++)sketch[i]=M-1;
   }
   void insert(int x){
+    assert(x>=0&&x<MAXU);
     sz++;
-    for(int i=0;i<K;i++)sketch[i]=std::min(sketch[i],(*sk).hash(i,x));
+    for(int i=0;i<K;i++)sketch[i]=std::min(sketch[i],(*h).hash(i,x));
   }
-  friend Set unite(Set a, Set b);
-  friend double compare(Set a, Set b);
+  friend Set unite(Set a, Set b);  //We need to access Set sketches
+  friend int compare(Set a, Set b);  //We need to access Set sketches
 
-  bool operator==(const  Set a)const{
-    if(a.sz!=sz)return false;
-    return compare(*this, a)==1.0;
+  bool operator==(const Set a)const{
+    if(a.sz!=sz)return false;  //increases accuracy
+    return compare(*this, a)==K;
   }
   int size(){return sz;}
-  Sketch* sketch_gen(){return sk;}
+  Hash* hash_gen(){return h;}
 };
 
-Set unite(Set a, Set b){ //generates AuB
-  assert(a.sketch_gen()==b.sketch_gen());
-  Set result(a.sketch_gen());
+Set unite(Set a, Set b){ //generates Set S=AuB
+  assert(a.hash_gen()==b.hash_gen());
+  Set result(a.hash_gen());
   for(int i=0;i<K;i++){
-    result.sketch[i]=std::min(a.sketch[i],b.sketch[i]);
+    result.sketch[i]=std::min(a.sketch[i],b.sketch[i]);  //min function has the associative propriety
   }
   result.sz=a.sz+b.sz;
   return result;
 }
 
-double compare(Set a, Set b){ //Jaccard similarity generator
-  double J=0;
+int compare(Set a, Set b){ //Jaccard similarity calculation (this value is multiplied by K to avoid floating-point problems)
+  assert(a.hash_gen()==b.hash_gen());
+  int J=0;
   for(int i=0;i<K;i++)J+=(a.sketch[i]==b.sketch[i]);
-  return J/K;
+  return J;
 }
