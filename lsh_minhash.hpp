8c5f0d2408571e468433b54c64f96114ae40cdaf
Alessandro <aleminipublic@gmail.com>
2021-12-07 14:11:44 +0100

    Remove global use of "std"

--- lsh_minhash.hpp
+++ lsh_minhash.hpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 #include "hash_table.hpp"
 
-using namespace std;
-
 const int BB = 1200;
 const int  RR = 10; //These parameters create a (0.5,0.6,0.69,0.12)-sensitive function
 const int D = 5;
@@ -13,22 +11,22 @@ private:
   HashTable OR[BB];
   Hash *h[BB];
   int sz;
-  vector<Set> sets[BB];
+  std::vector<Set> sets[BB];
 
 public:
   LSH(){
     sz=0;
     for(int i=0;i<B;i++)h[i]=new Hash(RR);
   }
-  void push(vector<int> v, int id){
+  void push(std::vector<int> v, int id){
       for(int i=0;i<B;i++){
         sets[i].push_back(Set(h[i],v,id));
         OR[i].insert(&sets[i].back());
       }
   }
 
-  set<int> query(vector<int> v){
-    set<int> ans;
+  std::set<int> query(std::vector<int> v){
+    std::set<int> ans;
     for(int i=0;i<B;i++){
       Set a(h[i],v);
       Vector sim=OR[i].similars(&a);
